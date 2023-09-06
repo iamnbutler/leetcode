ce7400499ca5e09a23709ec4797dc3afd97b6f13
Nate Butler <iamnbutler@gmail.com>
2023-09-05 23:28:08 -0400

    Add 2665

---
+++ 2665_counter_ii/solution.ts
@@ -0,0 +1,21 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let current_value = init
+    return {
+        increment: () => ++current_value,
+        decrement: () => --current_value,
+        reset: () => current_value = init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
